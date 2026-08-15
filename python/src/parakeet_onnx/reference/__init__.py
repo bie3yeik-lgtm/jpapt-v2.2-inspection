3e60e8baa46aab4f45021cf58c99a0c920a8801b
bie3yeik-lgtm <bie3yeik@gmail.com>
2026-08-16 08:17:10 +0900

    export Transformers ASR reference adapter

--- python/src/parakeet_onnx/reference/__init__.py
+++ python/src/parakeet_onnx/reference/__init__.py
@@ -1,9 +1,17 @@
 from .ctc import ctc_reference_logits
 from .nemo import NemoReference, load_pinned_nemo_model
 from .tdt import tdt_reference
+from .transformers import (
+    TransformersReferenceError,
+    TransformersReferenceOutput,
+    TransformersSpeechSeq2SeqReference,
+)
 
 __all__ = [
     "NemoReference",
+    "TransformersReferenceError",
+    "TransformersReferenceOutput",
+    "TransformersSpeechSeq2SeqReference",
     "ctc_reference_logits",
     "load_pinned_nemo_model",
     "tdt_reference",
