21569d2c328a2edcc6077848f355ef2670158f69
bie3yeik-lgtm <bie3yeik@gmail.com>
2026-08-16 13:16:17 +0900

    Export multi-decoder candidate finalizers

--- python/src/parakeet_onnx/export/__init__.py
+++ python/src/parakeet_onnx/export/__init__.py
@@ -1,10 +1,25 @@
 from .ctc import export_ctc_candidate
-from .metadata import CandidateMetadata, sha256_file, write_candidate_metadata
+from .finalize import finalize_candidate, load_runtime_contract
+from .metadata import (
+    ArtifactMetadata,
+    CandidateMetadata,
+    TokenizerMetadata,
+    sha256_file,
+    write_candidate_metadata,
+)
+from .tdt import export_tdt_candidate
 from .validate import validate_onnx_model
+from .whisper import export_whisper_candidate
 
 __all__ = [
+    "ArtifactMetadata",
     "CandidateMetadata",
+    "TokenizerMetadata",
     "export_ctc_candidate",
+    "export_tdt_candidate",
+    "export_whisper_candidate",
+    "finalize_candidate",
+    "load_runtime_contract",
     "sha256_file",
     "validate_onnx_model",
     "write_candidate_metadata",
