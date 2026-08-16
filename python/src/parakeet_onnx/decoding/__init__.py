8482b4bcbe072c2578459d1467a5a24bb4fa0dee
bie3yeik-lgtm <bie3yeik@gmail.com>
2026-08-16 17:16:43 +0900

    Remove obsolete TDT placeholder export

--- python/src/parakeet_onnx/decoding/__init__.py
+++ python/src/parakeet_onnx/decoding/__init__.py
@@ -1,11 +1,9 @@
 from .ctc import ctc_collapse, greedy_ctc_ids
 from .tokenizer import TokenizerAdapter
-from .tdt import TdtDecoderNotImplemented
 from .vocabulary import VocabularyTokenizer
 
 __all__ = [
     "TokenizerAdapter",
-    "TdtDecoderNotImplemented",
     "VocabularyTokenizer",
     "ctc_collapse",
     "greedy_ctc_ids",
