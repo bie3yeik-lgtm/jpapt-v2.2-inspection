7a3ee7577fe9d6d8a2e8c7f505fd98db4a287306
bie3yeik-lgtm <bie3yeik@gmail.com>
2026-08-16 13:13:31 +0900

    Export runtime registry APIs

--- python/src/parakeet_onnx/runtime/__init__.py
+++ python/src/parakeet_onnx/runtime/__init__.py
@@ -1,3 +1,15 @@
+from .adapter import AsrRuntimeAdapter, RuntimeTranscription
+from .artifacts import (
+    CandidateArtifact,
+    CandidateArtifacts,
+    CandidateMetadataError,
+    CandidateTokenizer,
+)
+from .factory import (
+    create_runtime_adapter,
+    register_runtime_factory,
+    registered_decoders,
+)
 from .inference import InferenceOutput, OrtCtcRunner
 from .model_contract import ModelContract, ModelContractError
 from .providers import (
@@ -9,15 +21,24 @@ from .session import OrtSessionConfig, create_session
 from .tensors import input_metadata, output_metadata
 
 __all__ = [
+    "AsrRuntimeAdapter",
+    "CandidateArtifact",
+    "CandidateArtifacts",
+    "CandidateMetadataError",
+    "CandidateTokenizer",
     "InferenceOutput",
     "ModelContract",
     "ModelContractError",
     "OrtCtcRunner",
     "OrtSessionConfig",
     "ProviderResolutionError",
+    "RuntimeTranscription",
     "available_provider_names",
+    "create_runtime_adapter",
     "create_session",
     "input_metadata",
     "output_metadata",
+    "register_runtime_factory",
+    "registered_decoders",
     "resolve_provider_chain",
 ]
